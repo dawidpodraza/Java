ae16bf5919ebef2e0f427661ab964dbc1a0b5a2b
Dawid <podrazad@gmail.com>
2024-10-25 19:50:32 +0200

    Modified Polimorfizm file and add Equals file

--- Polimorfizm/src/Person.java
+++ Polimorfizm/src/Person.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Person {
     private String firstName;
     private String lastName;
@@ -35,5 +37,18 @@ public class Person {
 
     public String getInfo(){
         return "Imie "+getFirstName()+" Nazwisko "+getLastName()+" Wypłata: "+getSalary();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Double.compare(salary, person.salary) == 0 && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, salary);
     }
 }
