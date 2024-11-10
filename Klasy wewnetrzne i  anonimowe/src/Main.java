434681f3fd07ec1c8003ffe6b51d778fca5d77f0
Dawid <podrazad@gmail.com>
2024-11-10 20:38:50 +0100

    Bubble sort in inner and anime classes

--- Klasy wewnetrzne i  anonimowe/src/Main.java
+++ Klasy wewnetrzne i  anonimowe/src/Main.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Main {
+    public static void main(String[] args) {
+
+        int[] table = {3435,6786,8907,21, 32, 12, 3, 5, 1, 19, 23,200,500,32,78};
+
+        Sortable sorter = new Sortable() {
+
+            public void sort(int[] tab) {
+                boolean changed = false;
+                for (int i = 0; i < tab.length - 1; i++) {
+                    int leftNumber = tab[i];
+                    int rightNumber = tab[i + 1];
+                    if (leftNumber > rightNumber) {
+                        tab[i] = rightNumber;
+                        tab[i + 1] = leftNumber;
+                        changed = true;
+                    }
+                    if (changed) {
+                        sort(tab);
+                    }
+                }
+            }
+        };
+        sorter.sort(table);
+        System.out.println(Arrays.toString(table));
+    }
 }
--- Klasy wewnetrzne i  anonimowe/src/Sortable.java
+++ Klasy wewnetrzne i  anonimowe/src/Sortable.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
-
 public interface Sortable {
+
+    void sort(int[] tab);
 }
