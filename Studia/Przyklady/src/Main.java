1569cab050099bc13512475c0e03c324eb760354
Dawid <podrazad@gmail.com>
2024-11-17 18:50:57 +0100

    Examples

--- Studia/Przyklady/src/Main.java
+++ Studia/Przyklady/src/Main.java
@@ -5,20 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        int index = 1;
-        int sum = 0;
-
-        while (index != 0) {
-            System.out.println("Podaj liczbę: ");
-            index = scanner.nextInt();
-            scanner.nextLine();
-            sum = sum + index;
-
-        }
-
-        System.out.println("Suma: " + sum);
-
+        
         
     }
 }
