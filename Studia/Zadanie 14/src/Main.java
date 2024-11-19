ba2f5a7984800e227b763e34fde689ec0a7396c8
Dawid <podrazad@gmail.com>
2024-11-19 19:53:36 +0100

    Resolved exercise 14

--- Studia/Zadanie 14/src/Main.java
+++ Studia/Zadanie 14/src/Main.java
@@ -1,20 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        System.out.println(gcd(5, 2));
+        System.out.println(nwd(6,3));
 
     }
+    public static int nwd(int a,int b){
+        while(a!=b){
+            if(a<b){
+                b=b-a;
+            }else{
+                a=a-b;
+            }
 
-    public static int gcd(int a, int b) {
-        while (b != 0) {
-            int c = a % b;
-            a = b;
-            b = c;
         }
         return a;
     }
-
-
-
 }
